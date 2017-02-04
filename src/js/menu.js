1688347d508a2f8b1ff133c1d0543d6ef4586f1b
Alan Sanchez <sanchezpineda03@gmail.com>
2017-02-04 16:32:22 -0600

    Navbar responsive styles and js logic.

---
+++ src/js/menu.js
@@ -0,0 +1,42 @@
+(function () {
+  var mobile = window.matchMedia('(max-width: 876px)');
+
+  function detectTouch(e) {
+    if (!$(e.target).is('nav .menu-trigger') && !$(e.target).is('nav .menu-container') && !$(e.target).closest('nav .menu-container').length && $('nav .menu-container').hasClass('active')) {
+      e.preventDefault();
+      $('nav .menu-container').removeClass('active');
+      $('body').removeClass('noscroll');
+    }
+  }
+
+  function detectClick() {
+    if ($('nav .menu-container').hasClass('active')) {
+      $('nav .menu-container').removeClass('active');
+      $('body').removeClass('noscroll');
+    } else {
+      $('nav .menu-container').addClass('active');
+      $('body').addClass('noscroll');
+    }
+  }
+
+  if (mobile.matches) {
+    $('body')[0].addEventListener('touchstart', detectTouch, {passive: false});
+    $('nav .menu-trigger').click(detectClick);
+  } else {
+    $('body')[0].removeEventListener('touchstart', detectTouch, {passive: false});
+    $('nav .menu-trigger').off('click');
+    $('body').removeClass('noscroll');
+  }
+
+  $(window).resize(function () {
+    $('body')[0].removeEventListener('touchmove', detectTouch, {passive: false});
+    $('nav .menu-trigger').off('click');
+    $('body').removeClass('noscroll');
+    $('nav .menu-container').removeClass('active');
+
+    if (mobile.matches) {
+      $('body')[0].addEventListener('touchmove', detectTouch, {passive: false});
+      $('nav .menu-trigger').click(detectClick);
+    }
+  });
+})();
